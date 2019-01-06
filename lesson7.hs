5b33b2a14c3703203cec36fea44cc79b3701714e
Leonhard Riedisser <leo008180@googlemail.com>
2019-01-06 20:53:14 +0100

    Lesson 7

---
+++ lesson7.hs
@@ -0,0 +1,28 @@
+gcd1 a b = if remainder == 0
+              then b
+              else gcd1 b remainder
+  where remainder = a `mod` b
+
+sayAmount n = case n of
+                   1 -> "one"
+                   2 -> "two"
+                   n -> "a bunch"
+
+myHead (x:xs) = x
+myHead [] = error "No head for empty list"
+
+myTail [] = []
+myTail (_:xs) = xs
+
+gcd2 a b = case remainder of
+                0 -> b
+                _ -> gcd2 b remainder
+  where remainder = a `mod` b
+
+gcd3 a 0 = a
+gcd3 a b = gcd3 b (a `mod` b)
+
+-- myTake 0 xs = []
+-- myTake n xs = [h, t]
+--   where h = head xs
+--         t = myTake (n-1) (tail xs)
